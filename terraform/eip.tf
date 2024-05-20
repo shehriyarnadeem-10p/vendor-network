b7f13b5c7095bb0121372b2e2ec4a9cfd39c7d5e
Shehriyar Nadeem <shehriyar.nadeem@10pearls.com>
2024-05-20 18:12:46 +0500

    VPC

--- terraform/eip.tf
+++ terraform/eip.tf
@@ -1,12 +1,12 @@
 resource "aws_eip" "eip_1" {
-    depends_on = {
+    depends_on = [
         aws_internet_gateway.igw
-    }
+    ]
 }
 
 
 resource "aws_eip" "eip_2" {
-    depends_on = {
+    depends_on = [
         aws_internet_gateway.igw
-    }
+    ]
 }
